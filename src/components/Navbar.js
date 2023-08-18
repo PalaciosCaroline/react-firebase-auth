e8204dabd0d11bf713d78e02cc8f8bf5b28f9f52
PalaciosCaroline <caroline2807@yahoo.fr>
2023-08-18 15:40:25 +0200

    installation des boutons de la navbar pour l'ouverture des modals

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,35 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../context/userContext';
 
 export default function Navbar() {
-  const {toggleModals} = useContext(UserContext);
-  
+  const { toggleModals } = useContext(UserContext);
+
   return (
     <nav className="navbar navbar-light bg-light px-4">
-        <Link to="/" className='navbar-brand'>AuthJS</Link>
-        <div>
-            <button className="btn btn-primary" onClick={() => toggleModals('signUp')}>Sign Up</button>
-            <button className="btn btn-primary ms-2">Sign In</button>
-            <button className="btn btn-danger ms-2">Log Out</button>
-        </div>
+      <Link to="/" className="navbar-brand">
+        AuthJS
+      </Link>
+      <div>
+        <button
+          onClick={() => toggleModals('signUp')}
+          className="btn btn-primary"
+        >
+          Sign Up
+        </button>
+        <button
+          onClick={() => toggleModals('signIn')}
+          className="btn btn-primary ms-2"
+        >
+          Sign In
+        </button>
+        <button
+          onClick={() => toggleModals('close')}
+          className="btn btn-danger ms-2"
+        >
+          Log Out
+        </button>
+      </div>
     </nav>
-  )
+  );
 }
